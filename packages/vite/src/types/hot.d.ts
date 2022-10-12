7bed81853d6bd775eb286485b94ae288ce588334
郑旭钡 <gfazba@gmail.com>
2022-10-12 17:40:27 +0800

    feat: HMR API

--- packages/vite/src/types/hot.d.ts
+++ packages/vite/src/types/hot.d.ts
@@ -6,14 +6,14 @@ export type ModuleNamespace = Record<string, any> & {
 
 export interface ViteHotContext {
   readonly data: any
-
-  accept(): void
-  accept(cb: (mod: ModuleNamespace | undefined) => void): void
-  accept(dep: string, cb: (mod: ModuleNamespace | undefined) => void): void
+  // 热更接收的模块，有四种定义：
+  accept(): void //没有参数，代表接收自身
+  accept(cb: (mod: ModuleNamespace | undefined) => void): void // mod是更新后的模块信息
+  accept(dep: string, cb: (mod: ModuleNamespace | undefined) => void): void // 监听单个依赖 dep 的更新
   accept(
     deps: readonly string[],
     cb: (mods: Array<ModuleNamespace | undefined>) => void
-  ): void
+  ): void // 监听多个依赖 deps 的更新
 
   acceptExports(exportNames: string | readonly string[]): void
   acceptExports(
@@ -21,10 +21,10 @@ export interface ViteHotContext {
     cb: (mod: ModuleNamespace | undefined) => void
   ): void
 
-  dispose(cb: (data: any) => void): void
+  dispose(cb: (data: any) => void): void // 清除任何更新导致的持久副作用
   decline(): void
-  invalidate(): void
-
+  invalidate(): void // 调用后直接刷新页面
+  /**模块监听热更事件 */
   on<T extends string>(
     event: T,
     cb: (payload: InferCustomEventPayload<T>) => void
